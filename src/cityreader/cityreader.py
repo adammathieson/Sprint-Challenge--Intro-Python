b710aebed08d6492e398dd97631201080d7c683c
Adam Mathieson <mathieson1222@gmail.com>
2019-10-04 10:17:39 -0700

    returning cities in proper format and test passing

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -1,6 +1,16 @@
 # Create a class to hold a city location. Call the class "City". It should have
 # fields for name, lat and lon (representing latitude and longitude).
+class City:
+  def __init__(self, name, lat, lon):
+    self.name = name
+    self.lat = lat
+    self.lon = lon
 
+  def __str__(self):
+    return "{}, {}, {}".format(self.name, self.lat, self.lon)
+
+# test = City('Test', 45, 100)
+# print(test)
 
 # We have a collection of US cities with population over 750,000 stored in the
 # file "cities.csv". (CSV stands for "comma-separated values".)
@@ -14,13 +24,23 @@
 #
 # Note that the first line of the CSV is header that describes the fields--this
 # should not be loaded into a City object.
+import csv
+
 cities = []
 
 def cityreader(cities=[]):
+  with open('cities.csv', 'r') as data_file:
+    csv_data = csv.reader(data_file)
+
+    #skip the first line of labels
+    next(csv_data)
+
   # TODO Implement the functionality to read from the 'cities.csv' file
   # For each city record, create a new City instance and add it to the 
   # `cities` list
-    
+    for line in csv_data:
+      cities.append(City(line[0], float(line[3]), float(line[4])))
+      # print(line[0], line[3], line[4])
     return cities
 
 cityreader(cities)
